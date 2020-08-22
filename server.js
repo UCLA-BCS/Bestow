2ee9c6978266f0d24640918724e97cb644b4906a
Mark F <the.mark.fullton@gmail.com>
2020-08-22 16:49:31 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+var db = require("./models");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.static(__dirname + "/build/"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./routes/bestowController")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
