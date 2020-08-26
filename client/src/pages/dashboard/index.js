22ca61d498db69893300b9bb9f21c0dd8d492625
Mark F <the.mark.fullton@gmail.com>
2020-08-25 18:14:37 -0700

    Added bootstrap cdn

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import OneTest from "./one.js";
 import TwoTest from "./two.js";
+import "../../assets/css/dashboard.css";
 
 const DashboardPage = (props) => {
   return (
-    <div className="dashboardPageWrapper">
+    <div className="alert alert-primary">
       <p>Dashboard Page</p>
       <Router>
         <Link to="/">First</Link>
