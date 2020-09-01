e52cb2984c8267691fb9bbbf5823a65d3a94e299
Mark F <the.mark.fullton@gmail.com>
2020-09-01 10:53:27 -0700

    Added basic form for user registration to test back end

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AddUser from "../../components/AddUser";
 
 const RegisterPage = (props) => {
   return (
     <div className="registerPageWrapper">
       <p>Register Page</p>
+      <AddUser />
     </div>
   );
 };
