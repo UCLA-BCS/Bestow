3cf22d08c575a313cb9cb7ab6d03577f81d76185
Juliana Hernandez <juliana88h@gmail.com>
2020-09-01 17:18:52 -0700

    updated server.js file and api route file. Also added test file to mess around with

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,25 +4,25 @@ const db = require("../models");
 const argon2 = require("argon2");
 const session = require("express-session");
 
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const mongooseConnection = mongoose.connection;
+// const mongooseConnection = mongoose.connection;
 
-// If there is an error while connecting, it logs the error
-mongooseConnection.on(
-  "error",
-  console.error.bind(console, "connection error:")
-);
+// // If there is an error while connecting, it logs the error
+// mongooseConnection.on(
+//   "error",
+//   console.error.bind(console, "connection error:")
+// );
 
-// When mongoose connects, it console logs that it's connected
-mongooseConnection.once("open", function () {
-  console.log("Connected to Database");
-});
+// // When mongoose connects, it console logs that it's connected
+// mongooseConnection.once("open", function () {
+//   console.log("Connected to Database");
+// });
 
 module.exports = (app) => {
   // ================================================================
---
+++ test.js
@@ -0,0 +1,31 @@
+function display(name){
+    console.log("Good to meet you",name)
+}
+display("Phil")
+display("Juliana")
+display("Bill")
+display("Mike")
+display("tony")
+
+
+// ----------------------------------------
+
+
+
+// <Display name="phil" hobby="walking" /> //parent level pass value to child
+
+// props{
+//     name:"phil",
+//     hobby:"walking"
+// }
+
+
+// function Display(props){
+//     return (
+//         <p>
+//            Hi there {props.name}. My hobby is {props.hobby}
+//         </p>
+//     )
+// }
+
+// export default Display
