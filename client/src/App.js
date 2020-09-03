f77b89d0d28cbbb4618690ec328363f60d85273e
Juliana Hernandez <juliana88h@gmail.com>
2020-09-02 22:57:19 -0700

    adding all component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavComponent from "./components/Navbar";
+import {Row, Col} from "reactstrap"
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavComponent />
+        <Row>
+          <Col md={12}>
+          <NavComponent />
+          </Col>
+        </Row>
+    
         <p>This shows up on every page</p>
         {/* <Route path="/" exact component={SplashPage} />
         <Route path="/login" component={LoginPage} />
--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Row, Col} from "reactstrap"
+import "./index.css"
+import { Button } from '@material-ui/core';
 
 const NavComponent = (props) => {
-  return (
-    <div className="navbar">
-      <p>
-        Bestow Temp Navbar ----) (<Link to="/">Home</Link> |{" "}
+  return( 
+  
+  
+  <>
+
+  
+  <div className="navbar header">
+
+<Row>
+  <Col md={6}>
+    <img src="/images/logo_v2.svg" className="img-responsive"  />  <Button variant="contained" color="secondary">
+  Hello
+</Button>
+  
+  </Col>
+</Row>
+
+      
+        {/* <Link to="/">Home</Link> |{" "}
         <Link to="/login">Login</Link> |{" "}
-        <Link to="/register">Create Account</Link> |{" "}
-        <Link to="/dashboard">Dashboard</Link> )
-      </p>
+        <Link to="/register">Create Account</Link> |{" "} */}
     </div>
+
+    </>
   );
 };
 
