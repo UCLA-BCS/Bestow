042221c45ff0c9c5e32ad90735a0b528403e13b7
Mark F <the.mark.fullton@gmail.com>
2020-09-04 11:38:00 -0700

    Added path to package.json

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
