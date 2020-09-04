be4e7850bbd8db279c0a03c0c677ec6cf466492e
Mark F <the.mark.fullton@gmail.com>
2020-09-03 21:03:59 -0700

    Added procfile and get route

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const session = require("express-session");
 var db = require("./models");
-var mongoose = require ("mongoose")
+var mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(path.join(__dirname + "client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require("./routes/apiRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bestow")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bestow");
 
-console.log(process.env.APIKey)
+app.get("*", (req, res) => {
+  res.sendfile(path.join((__dirname = "client/build/index.html")));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
