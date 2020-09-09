a3d5265ebccde868f164ad0b4af54fa98fd61eba
noisette <kasusky.music@gmail.com>
2020-09-09 15:24:21 -0700

    added comments on server.js for clarification

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Middleware defined here
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Static assets (for heroku)
 app.use(express.static(path.join(__dirname, "client/build")));
 
 require("./routes/apiRoutes")(app);
@@ -21,4 +24,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
+// Start the API server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
