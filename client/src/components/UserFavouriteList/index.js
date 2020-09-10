8297c69c5c5753510b2b1922a10d0eb0aa3808f4
noisette <kasusky.music@gmail.com>
2020-09-10 16:22:55 -0700

    latest edits for favourite page, might remove userfavouritelist component

--- client/src/components/UserFavouriteList/index.js
+++ client/src/components/UserFavouriteList/index.js
@@ -1,3 +1,33 @@
 // adding food list
 // possibility of creating classes for user, foodlist, etc for an easier translation between the database into the table
-// 
+import React from "react";
+import { Table } from "reactstrap";
+import "./style.css";
+
+const FavouriteList = (props) => {
+  return (
+    <Table hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Category</th>
+          <th>Shop</th>
+          <th>Name</th>
+          <th>Special Instructions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <th>#</th>
+          <td>Drink</td>
+          <td>Coffee Cat</td>
+          <td>Chai Tea latte</td>
+          <td>Oat Milk</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default FavouriteList;
