c3a0fd31842687d5007fee37f5208a001f1303c5
Juliana Hernandez <juliana88h@gmail.com>
2020-09-09 23:12:45 -0700

    added to server.js, apiRouts.js, API.js, and home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,15 +3,12 @@ import { Container, Row, Col } from "reactstrap";
 import Border from "../components/Border";
 import ProfileForm from "../components/UserProfile";
 
-function Home (props) {
-    return(
-<Border >
-          <Row></Row>
-          <p>Welcome to Bestow</p>
-          <ProfileForm /> Uncomment to see User Profile in box. Working on
-          setting up function
-       </Border>
-    )
-    }
-    
-    export default Home
+function Home(props) {
+  return (
+    <Border>
+      <ProfileForm />
+    </Border>
+  );
+}
+
+export default Home;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,16 @@ export default {
   //===============================================/
   // FAVOURITES
   //==============================================
+getCurrentUser: function (){
+return axios.get("/user")
+},
 
+signUp: function (userInfo){
+  return axios.post("/register",userInfo)
+},
+logIn: function (userInfo){
+  return axios.post("/login",userInfo)
+},
   // API.getFavourites(user).then((resp) => { //code })
   getFavourites: function (user) {
     return axios.get(`/favourite/getall/${user}`);
