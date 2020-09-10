7922bbf44370e68bb8ace069233971b0fecc95b7
noisette <kasusky.music@gmail.com>
2020-09-09 17:02:09 -0700

    server.js mimicking 11/server.js & edited Favourites Controller

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const path = require("path");
-var db = require("./models");
+
+// const path = require("path");
+// var db = require("./models");
+// 6-8 added from 11 react-router
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,18 +16,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Static assets (for heroku)
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// Add routes, both API and view
+app.use(routes);
 
-// Testing base backend
-app.get("/test", (req, res) => {
-  res.send("Works");
-});
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+// // Testing base backend
+// app.get("/test", (req, res) => {
+//   res.send("Works");
+// });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
+
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
 
 // Start the API server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
