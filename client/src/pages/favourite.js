199dec608a5c8d27680540e1595f9c498e19513b
noisette <kasusky.music@gmail.com>
2020-09-10 15:02:11 -0700

    new updates on Favourite page

---
+++ client/src/pages/favourite.js
@@ -0,0 +1,26 @@
+import React, {Component} from  "react";
+import { Container, Row, Col, Table } from "reactstrap";
+import RemoveFoodBtn from "../components/RemoveFoodBtn";
+import Border from "../components/Border";
+
+// function Favourite (props) {
+//     return(
+// <Border >
+//           <Row>
+//           <p>User's Favourite List</p>
+//           </Row>
+//        </Border>
+//     )
+// }
+
+class Favourite extends Component {
+    state = {
+        owner: "",
+        shop: "",
+        category: "",
+        name: "",
+        
+    }
+}
+    
+export default Favourite;
