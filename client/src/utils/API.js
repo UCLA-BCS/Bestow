213ca881a860fc10e6839b44761cc9e69749b759
Mark F <markafullton@outlook.com>
2020-09-09 17:03:49 -0700

    Added base utils for favourites

---
+++ client/src/utils/API.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+export default {
+  //===============================================/
+  // FAVOURITES
+  //==============================================
+
+  // API.getFavourites(user).then((resp) => { //code })
+  getFavourites: function (user) {
+    return axios.get(`/favourite/getall/${user}`);
+  },
+
+  // API.getSpecFavourite(favouriteId).then((resp) => { //code })
+  getSpecFavourite: function (favouriteId) {
+    return axios.get(`/favourite/get/${favouriteId}`);
+  },
+
+  // API.addFavourite(user, shop, category, name).then((resp) => { //code })
+  addFavourite: function (user, shop, category, name, specialInstructions) {
+    return axios.post({
+      url: "/favourite/add",
+      data: {
+        owner: user,
+        shop: shop,
+        category: category,
+        name: name,
+        specialInstructions: specialInstructions,
+      },
+    });
+  },
+
+  // API.updateFavourite(itemID, shop, category, name, specialInstructions).then((resp) => { //code })
+  updateFavourite: function (
+    itemID,
+    shop,
+    category,
+    name,
+    specialInstructions
+  ) {
+    return axios.post({
+      url: `/favourite/update/${itemID}`,
+      data: {
+        shop: shop,
+        category: category,
+        name: name,
+        specialInstructions: specialInstructions,
+      },
+    });
+  },
+
+  // API.deleteFavourite(trashID).then((resp) => { //code })
+  deleteFavourite: function (trashID) {
+    return axios.post(`/favourite/delete/${trashID}`);
+  },
+};
