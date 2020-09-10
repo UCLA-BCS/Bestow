cae65e018e661c451537bb85f435853ac1000e6b
Juliana Hernandez <juliana88h@gmail.com>
2020-09-09 18:16:00 -0700

    added updated app.js and created pages folder and files

---
+++ client/src/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container, Row, Col } from "reactstrap";
+import Border from"../components/Border";
+
+function Login (props) {
+    return(
+        <Border>
+            Login
+        </Border>
+    )
+}
+export default Login
