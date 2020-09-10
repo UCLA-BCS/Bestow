3829f944cb187ed5d410d42c9c986ec43387f2ef
noisette <kasusky.music@gmail.com>
2020-09-10 13:33:58 -0700

    using favourite instead of favorite

---
+++ client/src/pages/userfavourite.js
@@ -0,0 +1,15 @@
+import React from  "react";
+import { Container, Row, Col } from "reactstrap";
+import Border from "../components/Border";
+
+function Favourite (props) {
+    return(
+<Border >
+          <Row></Row>
+          <p>User Favourite List</p>
+
+       </Border>
+    )
+    }
+    
+    export default Favourite
