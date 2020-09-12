096fc7fee1d4c22d95c7cf4701f76fe5d888bdab
noisette <kasusky.music@gmail.com>
2020-09-12 10:45:44 -0700

    removed import and directly linked src in RemoveBtn component

--- client/src/components/RemoveBtn/index.js
+++ client/src/components/RemoveBtn/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./style.css";
-import removebutton from "/images/remove-food-btn.png";
-
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function RemoveBtn(props) {
   return (
-    <img src={ removebutton } role="button" tabIndex="0" onClick={() => props.removebtn(props.id)}/>
+    <img src="/images/remove-food-btn.png" role="button" tabIndex="0" onClick={() => props.removebtn(props.id)}/>
   );
 }
 
