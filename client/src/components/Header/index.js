d2a7cd7fc876b32a9b4452f1e1db68df4493d05a
Juliana Hernandez <juliana88h@gmail.com>
2020-09-17 21:52:12 -0700

    added delete button

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Row, Col } from "reactstrap";
 import "./index.css";
 
+
 const Header = (props) => {
   return (
     <>
@@ -11,7 +12,13 @@ const Header = (props) => {
           <Col md={12}>
             <Row>
               <Col md={2}>
-                <img src="/images/logo_v2.svg" className="img-responsive" />
+                <img src="/images/logo_v2.svg" className="img-responsive" style={{marginLeft: "12%"}}/>
+              </Col>
+              <Col md={10}>
+                {props.isLoggedIn ? <Link onClick={props.handleLogout} style={{color:"white", float:"right",marginTop:"8%", paddingRight:"20px"}}>
+                Logout
+                </Link>: ""}
+                
               </Col>
             </Row>
           </Col>
